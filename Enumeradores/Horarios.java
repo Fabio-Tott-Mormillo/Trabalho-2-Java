cbd06e051f4c068cb4f5d3b4f4040d1ecb0a5a5d
Fabio-Tott-Mormillo <79855517+Fabio-Tott-Mormillo@users.noreply.github.com>
2021-06-27 20:59:45 -0300

    Horarios dos funcionarios

--- Enumeradores/Horarios.java
+++ Enumeradores/Horarios.java
@@ -1,5 +1,11 @@
 package Enumeradores;
 
+/**
+ * Divide os Horários em 2, por dígito
+ * @param   Horarios    recebe um dígito (1 ou 2) e define a hora de serviço
+ * @return  retorna a hora de serviço
+ */
+
 public enum Horarios {
     NORMAL, EXTRA;
 }
