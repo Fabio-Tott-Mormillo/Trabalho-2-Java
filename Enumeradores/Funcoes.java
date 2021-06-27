96a8b1f77b14786d53e126ab2f75dd7aac193ca8
Fabio-Tott-Mormillo <79855517+Fabio-Tott-Mormillo@users.noreply.github.com>
2021-06-27 20:57:59 -0300

    Funcoes dos funcionarios

--- Enumeradores/Funcoes.java
+++ Enumeradores/Funcoes.java
@@ -1,5 +1,11 @@
 package Enumeradores;
 
+/**
+ * Divide as funções dos membros por dígitos
+ * @param   Funcoes     recebe um dígido (1, 2, 3 ou 4), e determina a função
+ * @return  retorna a função do membro
+ */
+
 public enum Funcoes {
     HeavyLifters, ScriptGuys, BigBrothers, MobileMembers   
 }
