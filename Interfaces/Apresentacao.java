67a2cfd945e3c00cff3bed364655b3f7872091eb
Fabio-Tott-Mormillo <79855517+Fabio-Tott-Mormillo@users.noreply.github.com>
2021-06-27 21:06:40 -0300

    Apresentacao interface

--- Interfaces/Apresentacao.java
+++ Interfaces/Apresentacao.java
@@ -1,5 +1,10 @@
 package Interfaces;
 
+/**
+ * Apresentação do funcionário quando o relatório for chamado
+ * @return  ele retorna os dados do funcionário
+ */
+
 public interface Apresentacao {
     public void show();
 }
