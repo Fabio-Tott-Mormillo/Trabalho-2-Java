1b5aac7d5ef50e5bb7f073b2658c7a8e96db12fd
Fabio-Tott-Mormillo <79855517+Fabio-Tott-Mormillo@users.noreply.github.com>
2021-06-27 21:07:45 -0300

    Cargo BigBrother

--- Models/BigBrother.java
+++ Models/BigBrother.java
@@ -3,6 +3,11 @@ package Models;
 import Funcionario.Funcionario;
 import Interfaces.PostarMensagem;
 
+/**
+ * Determina a assinatura a ser enviada (normal ou extra) para determinado membro
+ * @return  retorna a assinatura correspondente à característica de cada membro (citadas no material disponibilizado pelo professor)
+ */
+
 public class BigBrother extends Funcionario{
 
     public BigBrother(String nome, String email, String funcao) {
